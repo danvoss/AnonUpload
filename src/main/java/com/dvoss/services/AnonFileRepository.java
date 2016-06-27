14a2c214cf285dc560d3a41480904ab714752118
Dan <danielandrewvoss@gmail.com>
2016-06-27 12:17:31 -0400

    Add functionality

---
+++ src/main/java/com/dvoss/services/AnonFileRepository.java
@@ -0,0 +1,10 @@
+package com.dvoss.services;
+
+import com.dvoss.entities.AnonFile;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Dan on 6/27/16.
+ */
+public interface AnonFileRepository extends CrudRepository<AnonFile, Integer> {
+}
